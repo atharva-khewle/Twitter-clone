fd14873a513ca59ac19fdb1facc7af6ed939d263
Atharva khewle <120433600+discipline101@users.noreply.github.com>
2023-07-15 11:19:00 +0530

    Update ttr.js

--- ttr.js
+++ ttr.js
@@ -1,4 +1,7 @@
+document.getElementsByClassName('tweet2').addEventListener('hover', ()=>{
+    
+    document.getElementsByClassName('tweet2').style.color = 'red';
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+}
+
+);
